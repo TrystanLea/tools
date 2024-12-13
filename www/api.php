a595b8861016df9aea6199c3621f1e5d70a2e2b2
Trystan Lea <trystan.lea@gmail.com>
2024-12-13 20:41:29 +0000

    load data from cache if it exists

--- www/api.php
+++ www/api.php
@@ -20,7 +20,23 @@ if (!in_array($timeformat,array("unix","unixms","excel","iso8601","notime"))) {
     return array('success'=>false, 'message'=>'Invalid time format');
 }
 
-print file_get_contents("http://emoncms.org/feed/data.json?ids=$ids&start=$start&end=$end&interval=$interval&average=$average&delta=$delta&skipmissing=$skipmissing&limitinterval=$limitinterval&timeformat=$timeformat&dp=$dp");
+$url = "http://emoncms.org/feed/data.json?ids=$ids&start=$start&end=$end&interval=$interval&average=$average&delta=$delta&skipmissing=$skipmissing&limitinterval=$limitinterval&timeformat=$timeformat&dp=$dp";
+$md5_url = md5($url);
+
+// if cache exists load from cache
+$cache_file = "cache/$md5_url.json";
+
+if (file_exists($cache_file)) {
+    $data = file_get_contents($cache_file);
+    echo $data;
+    return;
+} else {
+    // if cache does not exist get data from emoncms
+    $data = file_get_contents($url);
+    // Save output to cache folder
+    // file_put_contents($cache_file,$data);
+    echo $data;
+}
 
 function get($index,$error_if_missing=false,$default=null)
 {
